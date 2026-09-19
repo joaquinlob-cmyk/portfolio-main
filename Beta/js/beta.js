a1b3290b743753036daf8b7384633ab62542c081
joaquinlob-cmyk <joaquin.lob@alumnos.cei.es>
2026-09-19 14:31:48 +0200

    commit

---
+++ Beta/js/beta.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll(".cursor-section");
+
+const cursor = document.createElement("img");
+cursor.classList.add("custom-cursor");
+
+document.body.appendChild(cursor);
+
+sections.forEach(section => {
+
+    section.addEventListener("mouseenter", () => {
+        cursor.src = section.dataset.cursor;
+        cursor.style.display = "block";
+    });
+
+    section.addEventListener("mousemove", (e) => {
+        cursor.style.left = `${e.clientX}px`;
+        cursor.style.top = `${e.clientY}px`;
+    });
+
+    section.addEventListener("mouseleave", () => {
+        cursor.style.display = "none";
+    });
+
+});
